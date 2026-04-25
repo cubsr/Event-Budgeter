ec5a39849ec64d2af2c146472f10ce47f12f0dd8
cubsr <97184952+cubsr@users.noreply.github.com>
2026-04-25 14:27:05 -0500

    Initial Commit

---
+++ Event Budgeter/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Event Budgeter
+//
+//  Created by Levi Greiner on 4/25/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Event BudgeterTests/Event_BudgeterTests.swift
@@ -0,0 +1,18 @@
+//
+//  Event_BudgeterTests.swift
+//  Event BudgeterTests
+//
+//  Created by Levi Greiner on 4/25/26.
+//
+
+import Testing
+
+struct Event_BudgeterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
